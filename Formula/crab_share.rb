e5451a31b892fb1a99d0783392d5fe06cbf22629
MayNiklas <info@niklas-steffen.de>
2024-02-14 22:33:30 +0100

    add crabshare

---
+++ Formula/crab_share.rb
@@ -0,0 +1,34 @@
+class CrabShare < Formula
+  desc "Simple application to upload files to an S3 bucket and receive a shareable link"
+  homepage "https://github.com/lounge-rocks/crab_share"
+
+  url "https://github.com/lounge-rocks/crab_share/archive/refs/tags/0.2.2.tar.gz"
+  sha256 "0cfa5c01b02c0fa0052f7031c229a269cff817d91a30bfa204c5e8853a900da0"
+
+  head "https://github.com/lounge-rocks/crab_share.git", branch: "main"
+
+  depends_on "rust" => :build
+  depends_on :macos
+
+  def install
+    system "cargo", "install", *std_cargo_args
+  end
+
+  test do
+    # only execute on stable builds
+    if build.stable?
+      # check if version matches the one in the formula
+      assert_match "crab_share #{version}", shell_output("#{bin}/crab_share --version")
+    end
+    # check if help text is printed
+    output = shell_output("#{bin}/crab_share --help")
+    assert_match "Usage:", output
+    # upload a test file - expect an error because no credentials are provided
+    (testpath/"test.txt").write("Hello World!")
+    output = shell_output("#{bin}/crab_share test.txt", 1)
+    assert_match "error reading credentials file: No such file or directory (os error 2)", output
+    # upload a test file that does not exist - expect an error
+    output = shell_output("#{bin}/crab_share file-does-not-exist.txt", 1)
+    assert_match "path does not exist", output
+  end
+end
