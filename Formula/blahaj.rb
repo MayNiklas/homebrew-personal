23bc3750e873e8816ec7eb8edbb8ac510924bbd1
Niklas <44636701+MayNiklas@users.noreply.github.com>
2024-08-01 13:04:56 +0000

    blahaj: add 2.2.0 bottle.

--- Formula/blahaj.rb
+++ Formula/blahaj.rb
@@ -5,6 +5,13 @@ class Blahaj < Formula
   sha256 "07f025c480f398ce9c83e20f411c825a923ea9cdc505bd40c7110b5f68dcd492"
   license "BSD-2-Clause"
 
+  bottle do
+    root_url "https://github.com/MayNiklas/homebrew-personal/releases/download/blahaj-2.2.0"
+    sha256 cellar: :any,                 arm64_sonoma: "a33db055261e1c04dbd3ed05df39229a750eb1007ddfaef9cbf01ecbc02146b3"
+    sha256 cellar: :any,                 ventura:      "9280e46d4dba2152be6976496486834623a86f02fc4dde9cc44763185c3e61bc"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "5b6d9e8296ac44f3a97513559378edecd8ba046e97234ebc7ec79d18d7273190"
+  end
+
   depends_on "crystal" => :build
   depends_on "bdw-gc"
   depends_on "libevent"
