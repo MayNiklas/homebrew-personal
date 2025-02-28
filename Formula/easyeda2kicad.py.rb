10d0cd5247fd8e5cb90762281ebe49be19d62cf9
MayNiklas <info@niklas-steffen.de>
2025-02-28 17:27:56 +0100

    fix test

--- Formula/easyeda2kicad.py.rb
+++ Formula/easyeda2kicad.py.rb
@@ -63,15 +63,8 @@ class Easyeda2kicadPy < Formula
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test easyeda2kicad.py`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system bin/"program", "do", "something"`.
-    system "false"
+    # check if help text is printed
+    output = shell_output("#{bin}/easyeda2kicad --help")
+    assert_match "-- easyeda2kicad.py", output
   end
 end
