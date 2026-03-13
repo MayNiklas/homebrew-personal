058269fe33bf698da7017cf65e9bff3ad1d1054b
MayNiklas <info@niklas-steffen.de>
2026-03-13 09:58:35 +0100

    bonn-mensa 1.3.0

--- Formula/bonn-mensa.rb
+++ Formula/bonn-mensa.rb
@@ -3,8 +3,8 @@ class BonnMensa < Formula
 
   desc "Meal plans for university canteens in Bonn"
   homepage "https://github.com/alexanderwallau/bonn-mensa"
-  url "https://github.com/alexanderwallau/bonn-mensa/archive/refs/tags/1.2.0.tar.gz"
-  sha256 "11086ed48f1aae111c53956f71f8e99d47dcf9f773e90a8e4c050e61e7d70119"
+  url "https://github.com/alexanderwallau/bonn-mensa/archive/refs/tags/1.3.0.tar.gz"
+  sha256 "3b06225ae712c9f7333a231459e2abeb99c5c1b2e0881d53b988eb1af2196ab8"
   license "MIT"
 
   head "https://github.com/alexanderwallau/bonn-mensa.git", branch: "main"
